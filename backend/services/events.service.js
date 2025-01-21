a19971406ae5cdca628287368d7183f7228c181c
Vlad4est <vlad.4est@gmail.com>
2025-01-21 13:12:46 +0200

    update 2

--- backend/services/events.service.js
+++ backend/services/events.service.js
@@ -1,74 +1,99 @@
 const EventModel = require("../data/events.model");
 const userService = require("./users.service");
 const mailService = require("./mail.service");
-//const axios = require("axios");
 const { v4: uuidv4 } = require('uuid');
 require("dotenv").config();
 
 const eventService = {
-    getEvents: async () => {
+    async getEvents() {
         return await EventModel.find();
     },
-    getEvent: async (eventId) => {
-        const foundEvent = await EventModel.findOne({id: eventId});
-        return foundEvent;
-    },
-    getEventsByOrganizerId: async (organizerId) => {
-      const events = await EventModel.find({organizerId: organizerId});
-      return events;
+
+    async getEvent(eventId) {
+        return await EventModel.findOne({id: eventId});
     },
 
-    createEvent: async (eventData) =>{
+    async getEventsByOrganizerId(organizerId) {
+        return await EventModel.find({organizerId: organizerId});
+    },
+
+    async createEvent(eventData) {
         eventData.id = uuidv4();
         const event = await EventModel.create(eventData);
         await userService.updateEventList(eventData.organizerId, eventData.id);
         return event;
-        console.log(event);
     },
 
-    deleteEvent: async(eventId) =>{
+    async deleteEvent(eventId) {
         return await EventModel.deleteOne({ id: eventId });
     },
-    reciveInvitationRequest: async(eventId, username) =>{
-      const event = await eventService.getEvent(eventId);
-      if(event.participants.includes(username)) {
-        await EventModel.updateOne({ id: eventId }, {$pull:  {pendingInvitations: username} });}
-      else {
-        await EventModel.updateOne({ id: eventId }, {$push:  {pendingInvitations: username} });
-        return
-      }
-  },
-  acceptRequest: async (eventId, organizerId, username) => {
-    console.log("am intrat service")
-   // console.log(eventId, organizerId, username);
-    const organizer = await userService.getUserById(organizerId);
-    const event = await eventService.getEvent(eventId);
-    const user = await userService.getUser(username);
-    if(organizer.isOrganizer && event.pendingInvitations.includes(username) && event.organizerId === organizerId) {
-      console.log("test-1")
-      await EventModel.updateOne({ id: eventId }, {$push:  {participants: username} });
-      await EventModel.updateOne({ id: eventId }, {$pull:  {pendingInvitations: username} });
-      console.log("test-2")
-      return await mailService.sendAcceptMail(username, eventId);
-    }
-    else{
-      console.log("test-3")
-    }
-    return false;
-  },
-  declineRequest: async (eventId, organizerId, username) => {
-    const organizer = await await userService.getUserById(organizerId);
-    const event = await eventService.getEvent(eventId);
-    const user = await userService.getUser(username);
-    if(organizer.isOrganizer && event.pendingInvitations.includes(username) && event.organizerId === organizerId){
-      await EventModel.updateOne({ id: eventId }, {$pull:  {pendingInvitations: username} });
-      await EventModel.updateOne({ id: eventId }, {$push:  {declinedInvitations: username} });
-      await mailService.sendDeclineMail(username, eventId);
-    }
-    
-  }
-    
 
-}
+    async reciveInvitationRequest(eventId, username) {
+        const event = await this.getEvent(eventId);
+        if(event.participants.includes(username)) {
+            await EventModel.updateOne(
+                { id: eventId }, 
+                { $pull: { pendingInvitations: username } }
+            );
+        } else {
+            await EventModel.updateOne(
+                { id: eventId }, 
+                { $push: { pendingInvitations: username } }
+            );
+        }
+    },
+
+    async acceptRequest(eventId, organizerId, username) {
+        const [organizer, event, user] = await Promise.all([
+            userService.getUserById(organizerId),
+            this.getEvent(eventId),
+            userService.getUser(username)
+        ]);
+
+        if (organizer.isOrganizer && 
+            event.pendingInvitations.includes(username) && 
+            event.organizerId === organizerId) {
+            
+            await Promise.all([
+                EventModel.updateOne(
+                    { id: eventId }, 
+                    {
+                        $push: { participants: username },
+                        $pull: { pendingInvitations: username }
+                    }
+                ),
+                mailService.sendEventEmail(user, event, true)
+            ]);
+            return true;
+        }
+        return false;
+    },
+
+    async declineRequest(eventId, organizerId, username) {
+        const [organizer, event, user] = await Promise.all([
+            userService.getUserById(organizerId),
+            this.getEvent(eventId),
+            userService.getUser(username)
+        ]);
+
+        if (organizer.isOrganizer && 
+            event.pendingInvitations.includes(username) && 
+            event.organizerId === organizerId) {
+            
+            await Promise.all([
+                EventModel.updateOne(
+                    { id: eventId }, 
+                    {
+                        $pull: { pendingInvitations: username },
+                        $push: { declinedInvitations: username }
+                    }
+                ),
+                mailService.sendEventEmail(user, event, false)
+            ]);
+            return true;
+        }
+        return false;
+    }
+};
 
 module.exports = eventService;
--- backend/services/mail.service.js
+++ backend/services/mail.service.js
@@ -1,66 +1,38 @@
+
 const nodemailer = require('nodemailer');
-const eventService = require("../services/events.service")
-const userService = require("../services/users.service")
+const userService = require("../services/users.service");
 
 const mailService = {
-    sendAcceptMail: async (username, eventId) => {
-        console.log("am intrat mail")
-        const user = await userService.getUser(username);
-        const event = await eventService.getEvent(eventId);
-        //const event = await eventService.getEvent(eventId);
+    async sendEventEmail(user, event, isAccept = true) {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             auth: {
                 user: 'youreventinvites@gmail.com',
                 pass: "pody kbsz ovik vakq"
             }
-        })
-    
-        const mailOptions = {
-            from: 'youreventinvites@gmail.com',
-            to: "vlad.4est@gmail.com",
-            subject: `Invitation to ${event.title}`,
-            text: `Hi ${user.username},\n\nYou have been invited to the event "${event.title}".\n\n- Description: ${event.description || "No description provided"}\n- Location: ${event.location}\n- Time: ${event.time}\n\nBest regards,\nThe Events Team`
-        }
-
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-                return false;
-            } else {
-                console.log('Email sent: ' + info.response);
-                return true;
-            }
-        })
-    },
-    sendDeclineMail: async (username, eventId) => {
-        const user = await userService.getUser(username);
-        const event = await eventService.getEvent(eventId);
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'youreventinvites@gmail.com',
-                pass: "pody kbsz ovik vakq"
-            }
-        })
+        });
 
         const mailOptions = {
             from: 'youreventinvites@gmail.com',
             to: "vlad.4est@gmail.com",
-            subject: 'Invitation Declined',
-            text: `Hi ${user.username},\n\nWe regret to inform you that your request to join the event "${event.title}" has been declined.`
-        }
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.log(error);
-                return false;
-            } else {
-                console.log('Email sent: ' + info.response);
-                return true;
-            }
-        })
-        
+            subject: isAccept ? `Invitation to ${event.title}` : 'Invitation Declined',
+            text: isAccept ?
+                `Hi ${user.username},\n\nYou have been invited to the event "${event.title}".\n\n- Description: ${event.description || "No description provided"}\n- Location: ${event.location}\n- Time: ${event.time}\n\nBest regards,\nThe Events Team` :
+                `Hi ${user.username},\n\nWe regret to inform you that your request to join the event "${event.title}" has been declined.`
+        };
+
+        return new Promise((resolve, reject) => {
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    console.log(error);
+                    reject(false);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    resolve(true);
+                }
+            });
+        });
     }
-}
+};
 
-module.exports = mailService
+module.exports = mailService;
